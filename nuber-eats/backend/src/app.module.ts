051a72ebd91be1da3cd23040e779e78224b3999a
baekCode <baekcode@gmail.com>
2021-05-13 23:03:08 +0900

    setting : auto schemafile 설정

--- nuber-eats/backend/src/app.module.ts
+++ nuber-eats/backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import {Module} from '@nestjs/common';
 import {GraphQLModule} from '@nestjs/graphql';
-import {join} from 'path';
-import { RestaurantsModule } from './restaurants/restaurants.module';
+import {RestaurantsModule} from './restaurants/restaurants.module';
 
 @Module({
   imports    : [GraphQLModule.forRoot({
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    autoSchemaFile: true
   }), RestaurantsModule],
   controllers: [],
   providers  : [],
