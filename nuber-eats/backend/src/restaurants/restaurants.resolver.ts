cc6ff2b0313f497acf1178d0dbcf080f42ca7a09
baekCode <baekcode@gmail.com>
2021-05-15 21:08:00 +0900

    feat : restaurnts service, injectable DB 조회

--- nuber-eats/backend/src/restaurants/restaurants.resolver.ts
+++ nuber-eats/backend/src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,16 @@
 import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
 import {Restaurant} from './entities/restaurant.entities';
 import {restaurantDto} from './dtos/restaurant.dto';
+import {RestaurantsService} from './restaurants.service';
 
 @Resolver(of => Restaurant)
 export class RestaurantsResolver {
+  constructor(private readonly restaurantService: RestaurantsService) {
+  }
+
   @Query(returns => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
   @Mutation(returns => Boolean)
