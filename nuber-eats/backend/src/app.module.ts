d8a1cfedcd280073f53aaa3155f0d2cfa2852267
baekCode <baekcode@gmail.com>
2021-05-15 19:12:11 +0900

    feat : Joi를 사용하여 환경변수 검증

--- nuber-eats/backend/src/app.module.ts
+++ nuber-eats/backend/src/app.module.ts
@@ -1,17 +1,19 @@
 import {Module} from '@nestjs/common';
 import {GraphQLModule} from '@nestjs/graphql';
-import {RestaurantsModule} from './restaurants/restaurants.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {ConfigModule} from '@nestjs/config';
-
-console.log(process.env.DB_PORT);
+import {RestaurantsModule} from './restaurants/restaurants.module';
+import * as Joi from 'joi';
 
 @Module({
   imports    : [
     ConfigModule.forRoot({
-      isGlobal     : true,
-      envFilePath  : process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod'
+      isGlobal        : true,
+      envFilePath     : process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile   : process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod')
+      })
     }),
     TypeOrmModule.forRoot({
       type       : 'postgres',
