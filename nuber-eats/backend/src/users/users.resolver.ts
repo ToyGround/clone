d57f41177f99ed3aafb12805820e3c65288e733b
baekCode <44539224+baekCode@users.noreply.github.com>
2021-05-16 23:49:22 +0900

    feat : User CreateAccount

--- nuber-eats/backend/src/users/users.resolver.ts
+++ nuber-eats/backend/src/users/users.resolver.ts
@@ -1,6 +1,7 @@
-import {Query, Resolver} from '@nestjs/graphql';
+import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
 import {User} from './entities/user.entity';
 import {UsersService} from './users.service';
+import {CreateAccountInput, CreateAccountOutput} from "./dtos/create-account-dto";
 
 @Resolver(of => User)
 export class UsersResolver {
@@ -10,5 +11,26 @@ export class UsersResolver {
   @Query(returns => Boolean)
   test() {
     return true;
+  }
+
+  @Mutation(returns => CreateAccountOutput)
+  async createAccount(@Args('input') createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
+    try{
+      const error = await this.usersService.createAccount(createAccountInput);
+      if(error) {
+        return {
+          ok: false,
+          error,
+        }
+      }
+      return {
+        ok: true
+      }
+    } catch (e) {
+      return {
+        ok: false,
+        error: e
+      }
+    }
   }
 }
--- nuber-eats/backend/src/users/users.service.ts
+++ nuber-eats/backend/src/users/users.service.ts
@@ -2,9 +2,26 @@ import {Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {User} from './entities/user.entity';
+import {CreateAccountInput} from "./dtos/create-account-dto";
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private readonly users: Repository<User>) {
+  }
+
+  async createAccount({email, password, role}: CreateAccountInput): Promise<string | undefined> {
+    // 새로운 유저 인지 체크
+    // 새로운 유저라면 계정을 생성하고 해쉬 패스워드
+    try {
+      const exists = await this.users.findOne({email})
+      if(exists) {
+        // 에러 발생 :: throw 에러 발생하는 방법도 있지만 에러인 경우 리턴을 시키는 방법으로 진행, resolver 에서 에러 처리
+        return 'There is a user with that email already'
+      }
+      await this.users.save(this.users.create({email, password, role}))
+    } catch (e) {
+      // 에러 발생 :: throw 에러 발생하는 방법도 있지만 에러인 경우 리턴을 시키는 방법으로 진행, resolver 에서 에러 처리
+      return "Couldn't create account"
+    }
   }
 }
