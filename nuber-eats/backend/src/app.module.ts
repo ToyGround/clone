bbaf2895df053db87b1c8d9002d76a7d40f2df68
baekCode <baekcode@gmail.com>
2021-05-16 12:37:38 +0900

    feat : User Model

--- nuber-eats/backend/src/app.module.ts
+++ nuber-eats/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import {Module} from '@nestjs/common';
+import * as Joi from 'joi';
 import {GraphQLModule} from '@nestjs/graphql';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {ConfigModule} from '@nestjs/config';
-import {RestaurantsModule} from './restaurants/restaurants.module';
-import * as Joi from 'joi';
-import {Restaurant} from './restaurants/entities/restaurant.entities';
+import {UsersModule} from './users/users.module';
+import {CommonModule} from './common/common.module';
+import {User} from './users/entities/user.entity';
 
 @Module({
   imports    : [
@@ -29,12 +30,13 @@ import {Restaurant} from './restaurants/entities/restaurant.entities';
       database   : process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging    : true,
-      entities   : [Restaurant]
+      entities   : [User]
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true
     }),
-    RestaurantsModule
+    UsersModule,
+    CommonModule
   ],
   controllers: [],
   providers  : [],
