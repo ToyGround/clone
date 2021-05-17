b54e594167c7d9677264f96aeb447f573cee35c3
baekCode <44539224+baekCode@users.noreply.github.com>
2021-05-17 21:52:53 +0900

    feat : bcrypt이용하여 hash password

--- nuber-eats/backend/src/users/entities/user.entity.ts
+++ nuber-eats/backend/src/users/entities/user.entity.ts
@@ -1,29 +1,41 @@
-import {Column, Entity} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import {BeforeInsert, Column, Entity} from 'typeorm';
 import {CoreEntity} from '../../common/entities/core.entity';
 import {Field, InputType, ObjectType, registerEnumType} from "@nestjs/graphql";
+import {InternalServerErrorException} from "@nestjs/common";
 
 enum UserRole {
-  Client,
-  Owner,
-  Delivery
+    Client,
+    Owner,
+    Delivery
 }
 
-registerEnumType(UserRole, {name:'UserRole'})
+registerEnumType(UserRole, {name: 'UserRole'})
 
 @InputType({isAbstract: true})
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
 
-  @Column()
-  @Field(type => String)
-  email: string;
+    @Column()
+    @Field(type => String)
+    email: string;
 
-  @Column()
-  @Field(type => String)
-  password: string;
+    @Column()
+    @Field(type => String)
+    password: string;
 
-  @Column({type : "enum", enum: UserRole})
-  @Field(type => UserRole)
-  role: UserRole;
+    @Column({type: "enum", enum: UserRole})
+    @Field(type => UserRole)
+    role: UserRole;
+
+    @BeforeInsert()
+    async hashPassword(): Promise<void> {
+        try {
+            this.password = await bcrypt.hash(this.password, 10)
+        } catch (e) {
+            console.log(e)
+            throw new InternalServerErrorException()
+        }
+    }
 }
