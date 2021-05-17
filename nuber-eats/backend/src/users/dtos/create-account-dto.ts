46807089922b00e8ee01fa623794dc3d3921156a
baekCode <44539224+baekCode@users.noreply.github.com>
2021-05-17 23:22:10 +0900

    feat : login 기능 구현

--- nuber-eats/backend/src/users/dtos/create-account-dto.ts
+++ nuber-eats/backend/src/users/dtos/create-account-dto.ts
@@ -1,15 +1,12 @@
 import {Field, InputType, ObjectType, PickType} from "@nestjs/graphql";
 import {User} from "../entities/user.entity";
+import {MutationOutput} from "../../common/dtos/output.dto";
 
 @InputType()
-export class CreateAccountInput extends PickType(User, ['email', 'password', 'role']){
+export class CreateAccountInput extends PickType(User, ['email', 'password', 'role']) {
 }
 
 @ObjectType()
-export class CreateAccountOutput {
-    @Field(type => String, {nullable:true})
-    error?: string
+export class CreateAccountOutput extends MutationOutput {
 
-    @Field(type => Boolean)
-    ok: boolean
 }
--- nuber-eats/backend/src/users/entities/user.entity.ts
+++ nuber-eats/backend/src/users/entities/user.entity.ts
@@ -3,11 +3,12 @@ import {BeforeInsert, Column, Entity} from 'typeorm';
 import {CoreEntity} from '../../common/entities/core.entity';
 import {Field, InputType, ObjectType, registerEnumType} from "@nestjs/graphql";
 import {InternalServerErrorException} from "@nestjs/common";
+import {IsEmail, IsEnum} from "class-validator";
 
 enum UserRole {
-    Client,
-    Owner,
-    Delivery
+	Client,
+	Owner,
+	Delivery
 }
 
 registerEnumType(UserRole, {name: 'UserRole'})
@@ -17,25 +18,36 @@ registerEnumType(UserRole, {name: 'UserRole'})
 @Entity()
 export class User extends CoreEntity {
 
-    @Column()
-    @Field(type => String)
-    email: string;
+	@Column()
+	@Field(type => String)
+	@IsEmail()
+	email: string;
 
-    @Column()
-    @Field(type => String)
-    password: string;
+	@Column()
+	@Field(type => String)
+	password: string;
 
-    @Column({type: "enum", enum: UserRole})
-    @Field(type => UserRole)
-    role: UserRole;
+	@Column({type: "enum", enum: UserRole})
+	@Field(type => UserRole)
+	@IsEnum(UserRole)
+	role: UserRole;
 
-    @BeforeInsert()
-    async hashPassword(): Promise<void> {
-        try {
-            this.password = await bcrypt.hash(this.password, 10)
-        } catch (e) {
-            console.log(e)
-            throw new InternalServerErrorException()
-        }
-    }
+	@BeforeInsert()
+	async hashPassword(): Promise<void> {
+		try {
+			this.password = await bcrypt.hash(this.password, 10)
+		} catch (e) {
+			console.log(e)
+			throw new InternalServerErrorException()
+		}
+	}
+
+	async checkPassword(aPassword: string): Promise<boolean> {
+		try {
+			return await bcrypt.compare(aPassword, this.password)
+		} catch (e) {
+			console.log(e)
+			throw new InternalServerErrorException()
+		}
+	}
 }
