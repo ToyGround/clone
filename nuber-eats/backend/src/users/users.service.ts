523bde89fb903b324def615693ccbbd45232b054
baekCode <44539224+baekCode@users.noreply.github.com>
2021-05-18 21:52:59 +0900

    feat : jwt sign

--- nuber-eats/backend/src/users/users.service.ts
+++ nuber-eats/backend/src/users/users.service.ts
@@ -1,18 +1,15 @@
-import * as jwt from 'jsonwebtoken'
 import {Injectable} from '@nestjs/common';
-import {ConfigService} from "@nestjs/config";
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {User} from './entities/user.entity';
-import {CreateAccountInput} from "./dtos/create-account-dto";
-import {LoginInput} from "./dtos/login.dto";
-import {JwtService} from "../jwt/jwt.service";
+import {CreateAccountInput} from './dtos/create-account-dto';
+import {LoginInput} from './dtos/login.dto';
+import {JwtService} from '../jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
 	constructor(
 		@InjectRepository(User) private readonly users: Repository<User>,
-		private readonly config: ConfigService,
 		private readonly jwtService: JwtService
 	) {
 	}
@@ -21,46 +18,46 @@ export class UsersService {
 		// 새로운 유저 인지 체크
 		// 새로운 유저라면 계정을 생성하고 해쉬 패스워드
 		try {
-			const exists = await this.users.findOne({email})
+			const exists = await this.users.findOne({email});
 			if (exists) {
 				// 에러 발생 :: throw 에러 발생하는 방법도 있지만 에러인 경우 리턴을 시키는 방법으로 진행, resolver 에서 에러 처리
-				return {ok: false, error: 'There is a user with that email already'}
+				return {ok: false, error: 'There is a user with that email already'};
 			}
-			await this.users.save(this.users.create({email, password, role}))
-			return {ok: true}
+			await this.users.save(this.users.create({email, password, role}));
+			return {ok: true};
 		} catch (e) {
 			// 에러 발생 :: throw 에러 발생하는 방법도 있지만 에러인 경우 리턴을 시키는 방법으로 진행, resolver 에서 에러 처리
-			return {ok: false, error: "Couldn't create account"}
+			return {ok: false, error: 'Couldn\'t create account'};
 		}
 	}
 
 	async login({email, password}: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
 		// email 유저 찾기
 		try {
-			const user = await this.users.findOne({email})
+			const user = await this.users.findOne({email});
 			if (!user) {
 				return {
 					ok   : false,
 					error: 'User not found'
-				}
+				};
 			}
-			const passwordCorrect = await user.checkPassword(password)
+			const passwordCorrect = await user.checkPassword(password);
 			if (!passwordCorrect) {
 				return {
 					ok   : false,
 					error: 'Wrong password'
-				}
+				};
 			}
-			const token = jwt.sign({id: user.id}, this.config.get('SECRET_KEY'))
+			const token = this.jwtService.sign(user.id);
 			return {
 				ok: true,
 				token
-			}
+			};
 		} catch (error) {
 			return {
 				ok: false,
 				error
-			}
+			};
 		}
 		// 찾은 유저의 password 확인
 		// JWT 만들고 user에게 주기
