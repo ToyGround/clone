cdafc3eb60a989e99e1b67587e739d81b915f6f8
baekCode <44539224+baekCode@users.noreply.github.com>
2021-05-19 10:22:35 +0900

    feat : authUser Decorator

--- nuber-eats/backend/src/users/users.resolver.ts
+++ nuber-eats/backend/src/users/users.resolver.ts
@@ -5,6 +5,7 @@ import {CreateAccountInput, CreateAccountOutput} from './dtos/create-account-dto
 import {LoginInput, LoginOutput} from './dtos/login.dto';
 import {UseGuards} from '@nestjs/common';
 import {AuthGuard} from '../auth/auth.guard';
+import {AuthUser} from '../auth/auth-user.decorator';
 
 @Resolver(of => User)
 export class UsersResolver {
@@ -42,11 +43,7 @@ export class UsersResolver {
 
 	@Query(returns => User)
 	@UseGuards(AuthGuard)
-	me(@Context() context) {
-		if (!context.user) {
-			return;
-		} else {
-			return context.user;
-		}
+	me(@AuthUser() authUser: User) {
+		return authUser;
 	}
 }
