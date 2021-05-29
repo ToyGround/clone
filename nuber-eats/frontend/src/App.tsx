11aa7c939919ce0dbc02d85ae1e8d29bc58f785a
baekCode <baekcode@gmail.com>
2021-05-29 20:47:24 +0900

    add : front setting, tailwindcss

---
+++ nuber-eats/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-xl text-center text-white">Hello My App</h1>
+    </div>
+  );
+}
+
+export default App;
