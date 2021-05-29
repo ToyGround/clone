c9665e465ef6f15c984c4192540c88364f2519b0
baekCode <baekcode@gmail.com>
2021-05-29 23:07:45 +0900

    feat : apollo "reactive variables"를 이용하여 graphql 상태 변경(리덕스를 사용안해도 됨..)

--- nuber-eats/frontend/src/apollo.ts
+++ nuber-eats/frontend/src/apollo.ts
@@ -1,15 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {ApolloClient, InMemoryCache, makeVar} from '@apollo/client';
+
+/**
+ * apollo, graphql 상태 변경을 위한 방법 "reactive variables"
+ * apollo.ts -> isLoggedInVar 참고
+ * 레퍼런스 : https://www.apollographql.com/docs/react/local-state/managing-state-with-field-policies/#storing
+ * */
+export const isLoggedInVar = makeVar(false);
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri  : 'http://localhost:4000/graphql',
   cache: new InMemoryCache({
-    typePolicies : {
-      Query : {
-        fields : {
-          isLoggedIn : {
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
             read() {
               // return Boolean(localStorage.getItem('token'))
-              return false
+              return isLoggedInVar();
             }
           }
         }
@@ -18,4 +25,4 @@ const client = new ApolloClient({
   })
 });
 
-export default client
+export default client;
--- nuber-eats/frontend/src/routers/logged-out-router.tsx
+++ nuber-eats/frontend/src/routers/logged-out-router.tsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {isLoggedInVar} from '../apollo';
 
-export default function LoggedOutRouter () {
-  return <>LoggedOutRouter</>
+export default function LoggedOutRouter() {
+
+  /**
+   * apollo, graphql 상태 변경을 위한 방법 "reactive variables"
+   * apollo.ts -> isLoggedInVar 참고
+   * 레퍼런스 : https://www.apollographql.com/docs/react/local-state/managing-state-with-field-policies/#storing
+   * */
+  const onClick = () => {
+    isLoggedInVar(true);
+  };
+  return (
+    <>
+      <h1>LoggedOutRouter</h1>
+      <button onClick={onClick}>Click to Login</button>
+    </>
+  );
 }
