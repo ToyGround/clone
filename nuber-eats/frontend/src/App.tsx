167bb3aaf8aedd7727849ab3cee264ddefc6eda3
baekCode <baekcode@gmail.com>
2021-05-29 23:10:34 +0900

    refactor : 기존 apollo -> reactive variables, useReactiveVar 를 이용해서 코드 개선

--- nuber-eats/frontend/src/App.tsx
+++ nuber-eats/frontend/src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import {gql, useQuery} from '@apollo/client';
+import {useReactiveVar} from '@apollo/client';
 import LoggedOutRouter from './routers/logged-out-router';
 import LoggedInRouter from './routers/logged-in-router';
-
-const IS_LOGGED_IN = gql`
-  query isLoggedIn {
-  isLoggedIn @client
-  }
-`;
+import {isLoggedInVar} from './apollo';
 
 function App() {
-  const {data: {isLoggedIn}} = useQuery(IS_LOGGED_IN);
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn ? <LoggedInRouter/> : <LoggedOutRouter/>;
 }
 
