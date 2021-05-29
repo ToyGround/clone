902900bb3eb8ec1b4d709f36edf410d7d3300b52
baekCode <baekcode@gmail.com>
2021-05-29 23:12:46 +0900

    feat : logged in router, click handler

--- nuber-eats/frontend/src/routers/logged-in-router.tsx
+++ nuber-eats/frontend/src/routers/logged-in-router.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {isLoggedInVar} from '../apollo';
 
-export default function LoggedInRouter () {
-  return <>LoggedInRouter</>
+export default function LoggedInRouter() {
+  const onClick = () => {
+    isLoggedInVar(false);
+  };
+  return (
+    <>
+      <h1>Logged In Router</h1>
+      <button onClick={onClick}>Click to Logout</button>
+    </>
+  );
 }
