6a29edf3e1429881c10fa1712be1645d8fb7268e
baekCode <baekcode@gmail.com>
2021-05-30 11:23:53 +0900

    feat : useForm(react-hook-form), handleSubmit 기본사용법

--- nuber-eats/frontend/src/routers/logged-out-router.tsx
+++ nuber-eats/frontend/src/routers/logged-out-router.tsx
@@ -6,9 +6,12 @@ export default function LoggedOutRouter() {
   /**
    * [react-hook-form]
    * watch : register 로 등록된 부분을 감지한다.
+   * handleSubmit : handleSubmit 이용하여 onSubmit 함수를 넣어준다. (e.preventDefault()를 안해도 된다.)
    * */
-  const {register, watch} = useForm();
-  console.log(watch());
+  const {register, watch, handleSubmit} = useForm();
+  const onSubmit = () => {
+    console.log(watch('email'));
+  };
 
   /**
    * apollo, graphql 상태 변경을 위한 방법 "reactive variables"
@@ -21,9 +24,10 @@ export default function LoggedOutRouter() {
   return (
     <>
       <h1>LoggedOutRouter</h1>
-      <form>
-        <div><input {...register('email')} type="email" name="email" placeholder="email"/></div>
-        <div><input {...register('password')} type="password" name="password" placeholder="password"/></div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div><input {...register('email', {required: true})} type="email" name="email" placeholder="email"/></div>
+        <div><input {...register('password', {required: true})} type="password" name="password" placeholder="password"/>
+        </div>
         <button className={'bg-blue-500 text-white'}>Click to Login</button>
       </form>
     </>
