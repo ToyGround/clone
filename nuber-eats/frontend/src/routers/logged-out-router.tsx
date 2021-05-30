a467418c0d7ade4d51c2d13f90c44d43fe1375d9
baekCode <baekcode@gmail.com>
2021-05-30 10:54:52 +0900

    feat : react-form-hook, 기본사용방법 구현

--- nuber-eats/frontend/src/routers/logged-out-router.tsx
+++ nuber-eats/frontend/src/routers/logged-out-router.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import {isLoggedInVar} from '../apollo';
+import {useForm} from 'react-hook-form';
 
 export default function LoggedOutRouter() {
+  /**
+   * [react-hook-form]
+   * watch : register 로 등록된 부분을 감지한다.
+   * */
+  const {register, watch} = useForm();
+  console.log(watch());
 
   /**
    * apollo, graphql 상태 변경을 위한 방법 "reactive variables"
@@ -14,7 +21,11 @@ export default function LoggedOutRouter() {
   return (
     <>
       <h1>LoggedOutRouter</h1>
-      <button onClick={onClick}>Click to Login</button>
+      <form>
+        <div><input {...register('email')} type="email" name="email" placeholder="email"/></div>
+        <div><input {...register('password')} type="password" name="password" placeholder="password"/></div>
+        <button className={'bg-blue-500 text-white'}>Click to Login</button>
+      </form>
     </>
   );
 }
