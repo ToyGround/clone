b158aea94bf97065b5941d6141357a52e4391a7e
baekCode <baekcode@gmail.com>
2021-06-08 00:00:59 +0900

    feat : 로그인 성공시 apollo client, login 상태 변경

--- nuber-eats/frontend/src/pages/login.tsx
+++ nuber-eats/frontend/src/pages/login.tsx
@@ -5,7 +5,8 @@ import {Link} from 'react-router-dom';
 import FormError from '../components/FormError';
 import Button from '../components/Button';
 import {LoginMutation, LoginMutationVariables} from '../__generated__/LoginMutation';
-import { Helmet } from 'react-helmet-async';
+import {Helmet} from 'react-helmet-async';
+import {isLoggedInVar} from '../apollo';
 
 const LOGIN_MUTATION = gql`
   mutation LoginMutation($loginInput:LoginInput!) {
@@ -31,6 +32,7 @@ export default function Login() {
     const {login: {ok, token}} = data;
     if (ok) {
       console.log(token);
+      isLoggedInVar(true);
     }
   };
 
@@ -62,14 +64,23 @@ export default function Login() {
       <div className={'bg-white px-10 w-full max-w-lg py-10 rounded-md text-center'}>
         <h3 className={'text-2xl text-gray-800'}>Login</h3>
         <form onSubmit={handleSubmit(onSubmit)} className={'grid gap-3 p-5'}>
-          <input {...register('email', {required: '이메일을 입력해주세요.', pattern: {value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, message:'이메일 형식으로 입력해주세요.'}})}
+          <input {...register('email', {
+            required: '이메일을 입력해주세요.',
+            pattern : {
+              value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: '이메일 형식으로 입력해주세요.'
+            }
+          })}
                  name="email"
                  type="email"
                  required
                  className={'login-input transition300 mb-5'}
                  placeholder="Email"/>
           {errors.email?.message && <FormError errorMessage={errors.email.message}/>}
-          <input {...register('password', {required: '비밀번호를 입력해주세요.', minLength: 8})}
+          <input {...register('password', {
+            required : '비밀번호를 입력해주세요.',
+            minLength: 8
+          })}
                  name="password"
                  type="password"
                  required
