84b06c090bd41a50116921be116f73c33d19793f
baekCode <baekcode@gmail.com>
2021-06-08 00:03:21 +0900

    feat : signup 계정 생성 로직 구현 - 생성시 데이터 값 전달 onSubmit - 생성 콜 응 답값이 성공이면 history 이용하여 로그인 페이지 이동 - 생성 콜 응답값이 실패 면 실패 메세지 처리

--- nuber-eats/frontend/src/pages/signup.tsx
+++ nuber-eats/frontend/src/pages/signup.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {gql, useMutation} from '@apollo/client';
 import {useForm} from 'react-hook-form';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import FormError from '../components/FormError';
 import Button from '../components/Button';
-import { Helmet } from 'react-helmet-async';
+import {Helmet} from 'react-helmet-async';
 import {UserRole} from '../__generated__/globalTypes';
+import {createAccountMutation, createAccountMutationVariables} from '../__generated__/createAccountMutation';
 
 const CREATE_ACCOUNT_MUTATION = gql`
   mutation createAccountMutation($createAccountInput:CreateAccountInput!) {
@@ -19,22 +20,39 @@ const CREATE_ACCOUNT_MUTATION = gql`
 interface ICreateAccountForm {
   email: string;
   password: string;
-  role :UserRole
+  role: UserRole
 }
 
 export default function Signup() {
-  const {register, getValues,watch, handleSubmit, formState: {errors, isValid}} = useForm<ICreateAccountForm>({
-    mode: 'onChange',
-    defaultValues : {
-      role : UserRole.Client
+  const {register, getValues, handleSubmit, formState: {errors, isValid}} = useForm<ICreateAccountForm>({
+    mode         : 'onChange',
+    defaultValues: {
+      role: UserRole.Client
     }
   });
+  const history = useHistory();
+  const onCompleted = (data: createAccountMutation) => {
+    const {createAccount: {ok}} = data;
+    if (ok) {
+      history.push('/login');
+    }
+  };
+  const [crateAccountMutation, {loading, data: createAccountMutationResult}] = useMutation<createAccountMutation, createAccountMutationVariables>(CREATE_ACCOUNT_MUTATION, {onCompleted});
 
-  const [crateAccountMutation] = useMutation(CREATE_ACCOUNT_MUTATION);
-
-  const onSubmit = () => {};
-
-  console.log(watch())
+  const onSubmit = () => {
+    if (!loading) {
+      const {email, password, role} = getValues();
+      crateAccountMutation({
+        variables: {
+          createAccountInput: {
+            email,
+            password,
+            role
+          }
+        }
+      });
+    }
+  };
 
   return (
     <div className={'h-screen flex items-center justify-center bg-gray-800'}>
@@ -42,14 +60,23 @@ export default function Signup() {
       <div className={'bg-white px-10 w-full max-w-lg py-10 rounded-md text-center'}>
         <h3 className={'text-2xl text-gray-800'}>Signup</h3>
         <form onSubmit={handleSubmit(onSubmit)} className={'grid gap-3 p-5'}>
-          <input {...register('email', {required: '이메일을 입력해주세요.', pattern: {value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, message:'이메일 형식으로 입력해주세요.'}})}
+          <input {...register('email', {
+            required: '이메일을 입력해주세요.',
+            pattern : {
+              value  : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: '이메일 형식으로 입력해주세요.'
+            }
+          })}
                  name="email"
                  type="email"
                  required
                  className={'login-input transition300 mb-5'}
                  placeholder="Email"/>
           {errors.email?.message && <FormError errorMessage={errors.email.message}/>}
-          <input {...register('password', {required: '비밀번호를 입력해주세요.', minLength: 8})}
+          <input {...register('password', {
+            required : '비밀번호를 입력해주세요.',
+            minLength: 8
+          })}
                  name="password"
                  type="password"
                  required
@@ -57,10 +84,12 @@ export default function Signup() {
                  placeholder="Password"/>
           {errors.password?.message && <FormError errorMessage={errors.password.message}/>}
           {errors.password?.type === 'minLength' && <FormError errorMessage={'비밀번호는 8자 이상입니다.'}/>}
-          <select {...register('role',{required:true})}  className={'account-select'}>
+          <select {...register('role', {required: true})} className={'account-select'}>
             {Object.keys(UserRole).map((role) => <option key={role}>{role}</option>)}
           </select>
-          <Button isClickOn={isValid} loading={false} actionText={'CREATE ACCOUNT'}/>
+          {createAccountMutationResult?.createAccount.error &&
+          <FormError errorMessage={createAccountMutationResult.createAccount.error}/>}
+          <Button isClickOn={isValid} loading={loading} actionText={'CREATE ACCOUNT'}/>
         </form>
         <p>이미 회원이신가요? <Link to={'/login'} className={'text-blue-400 hover:underline'}>로그인 바로가기</Link></p>
       </div>
